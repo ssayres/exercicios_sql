0b8a0c07f4ab31684c93cfa35436354fb80ee15b
Sidnei Ayres <a.sidnei@aluno.ifsp.edu.br>
2022-03-14 23:56:53 -0300

    commit do file

---
+++ consultas.php
@@ -0,0 +1,149 @@
+<? include_once 'conecta.php' ?>
+
+<?php  
+/* ADENDO * FIZ ESSA FILE POIS ACHEI QUE FICARIA MAIS FACIL DE  SUBIR NO GITHUB *
+
+1- INNERJOIN - BUSCA UMA COLUNA EM OUTRA TABELA, ATRAVÉS DE UMA KEY EM COMUM (UMA PRIMARY E UMA FOREIGN)
+SELECT 
+productCode, productName,textDescription
+FROM
+    classicmodels.products tabel1
+    INNER JOIN classicmodels.productlines tabel2 
+   ON tabel1.productLine = tabel2.productLine; 
+
+ 2 - INNER JOIN/ USING - FORMA CURTA DO INNER JOIN DEPOIS QUE O MÉTODO JÁ FOI INSTANCIADO 
+ SELECT 
+    productCode, 
+    productName, 
+    textDescription
+FROM
+    classicmodels.products
+INNER JOIN classicmodels.productlines USING (productLine);
+
+3- INNER JOIN / GROUP BY - usa a key em comum orderNumber para ordenar a busca entre as duas tabelas
+FUNÇÃO DE AGREGAÇÃO SUM
+
+SELECT
+  t1.orderNumber,
+    t1.status,
+    SUM(quantityOrdered * priceEach) total
+FROM
+    classicmodels.orders t1
+ INNER JOIN
+ classicmodels.orderdetails t2
+
+    ON t1.orderNumber = t2.orderNumber
+GROUP BY orderNumber;
+
+4 - GROUP BY/ USING - FORMA REDUZIDA UMA VEZ QUE O GROUP BY FOI INSTANCIADO 
+ SELECT 
+    orderNumber,
+    status,
+    SUM(quantityOrdered * priceEach) total
+FROM
+    classicmodels.orders
+INNER JOIN classicmodels.orderdetails USING (orderNumber)
+GROUP BY orderNumber;
+
+5 - ORDER BY - INNER JOIN ORDENADO COMO O GROUP BY PORÉM ENTRE TRES TABELAS
+,
+    orderLineNumber,
+    productName,
+    quantityOrdered,
+    priceEach
+FROM
+    classicmodels.orders
+INNER JOIN
+    classicmodels.orderdetails USING (orderNumber)
+INNER JOIN
+    classicmodels.products USING (productCode)
+ORDER BY 
+    orderNumber, 
+    orderLineNumber;
+
+6 - INNER JOIN - FILTRANDO VALORES POR DIFERENÇA 
+SELECT 
+    orderNumber, 
+    productName, 
+    msrp, 
+    priceEach
+FROM
+    classicmodels.products p
+INNER JOIN classicmodels.orderdetails o 
+   ON p.productcode = o.productcode
+      AND p.msrp > o.priceEach
+WHERE
+    p.productcode = 'S10_1678';
+
+ 7 - INNER JOIN - BUSCANDO POR PARÂMETROS DIFERENTES 
+ SELECT 
+    firstName, 
+    city, 
+    postalCode
+FROM
+    classicmodels.employees t1
+INNER JOIN classicmodels.offices t2 
+    ON t1.officeCode = t2.officeCode;      
+
+8 - INNER JOIN - QUATRO TABELAS
+SELECT 
+    orderNumber,
+    orderDate,
+    customerName,
+    orderLineNumber,
+    productName,
+    quantityOrdered,
+    priceEach
+FROM
+    classicmodels.orders
+INNER JOIN classicmodels.orderdetails 
+    USING (orderNumber)
+INNER JOIN classicmodels.products 
+    USING (productCode)
+INNER JOIN classicmodels.customers 
+    USING (customerNumber)
+ORDER BY 
+    orderNumber, 
+    orderLineNumber;
+
+9 - EXISTS - busca pela condição de um cliente que fez ao menos três pedidos
+SELECT 
+    customerNumber, 
+    customerName
+FROM
+    classicmodels.customers
+WHERE
+    EXISTS(
+	SELECT 
+            3
+        FROM
+           classicmodels.orders
+        WHERE
+            orders.customernumber 
+		= customers.customernumber); 
+ 
+10 - IS NULL - BUSCA PELA VALIDAÇÃO DE VALORES NÃO NULOS
+ SELECT  
+    city, 
+    postalCode
+FROM
+    classicmodels.offices
+postalCode IS NULL 
+ORDER BY 
+postalCode;        
+
+11 - HAVING - PARECIDO COM GROUP BY - NESTE CASO BUSCA O NUMERO DOS PEDIDOS NUMERO DE VENDAS POR PEDIDO
+E TOTAL DE VENDAS PRA CADA orderdetails :
+
+SELECT 
+    ordernumber,
+    SUM(quantityOrdered) AS itemsCount,
+    SUM(priceeach*quantityOrdered) AS total
+FROM
+    classicmodels.orderdetails
+GROUP BY ordernumber;
+HAVING 
+   total > 1000;
+    */ 
+
+    ?>
